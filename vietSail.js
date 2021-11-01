6ab3b99f072c6750f0070780669b5a4cfee11175
haiCocoBar <daihai971@yahoo.fr>
2021-11-01 17:15:06 +0700

    load

--- vietSail.js
+++ vietSail.js
@@ -1,3 +1,22 @@
+//-----NAVBAR-SCROLL----//
+window.addEventListener('scroll', function () {
+		
+    if (window.pageYOffset > 100) {
+	      $('.navbar').addClass("bg-light");
+			} else {
+        $('.navbar').removeClass("bg-light");  
+			}
+      
+    var bottomViewPort = $(window).scrollTop() + $(window).height();
+    var footerTop = $('#topFooter').offset().top;
+
+    if (bottomViewPort >= footerTop) {
+      $('.icon-bar').addClass("hide");
+    } else {
+      $('.icon-bar').removeClass("hide");
+    }
+		});
+  
 //------PICTURE-SIZE-ONCLICK-----//
 //$(document).ready(function () {
 //    var small={width: "300px",height: "185px"};
@@ -11,43 +30,43 @@
 
 //------READ-MORE-OR-LESS-----//
 function myFunction() {
-    var dots = document.getElementById("dots");
-    var moreText = document.getElementById("more");
-    var btnText = document.getElementById("myBtn");
-  
-    if (dots.style.display === "none") {
-      dots.style.display = "inline";
-      btnText.innerHTML = "Read more";
-      moreText.style.display = "none";
-    } else {
-      dots.style.display = "none";
-      btnText.innerHTML = "Read less";
-      moreText.style.display = "inline";
-    }
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
   }
+}
+
+//-----HIDE-SOCIAL-MEDIA-BAR----//
+// $(document).ready(function () {
+
+//   $(document).scroll(function () {
+
+//     var footerSelector = '#topFooter';
+//     var socialBarSelector = '.icon-bar';
+
+//     var bottomViewPort = $(window).scrollTop() + $(window).height();
+//     var footerTop = $(footerSelector).offset().top;
+
+//     if (bottomViewPort >= footerTop) {
+//       $(socialBarSelector).addClass("hide");
+//     } else {
+//       $(socialBarSelector).addClass("show");
+//     }
+//   });
+
+// });
 
 
-  $(document).ready(function(){
 
-    $(document).scroll(function(){
-  
-      var footerSelector = '#topFooter';
-      var socialBarSelector = '.icon-bar';
-  
-      var bottomViewPort = $(window).scrollTop()+$(window).height();
-      var footerTop = $(footerSelector ).offset().top;
-  
-      if( bottomViewPort>=footerTop){
-       $(socialBarSelector).fadeOut();
-     }else{
-        $(socialBarSelector).fadeIn(); 
-      }
-    });
-  
-  });
-
-
- 
 
 
 
