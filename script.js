a76708978540b2b08577f89614f68b4dbc881a90
Khoa Quach <khoa@niftit.com>
2022-04-01 04:10:02 +0700

    fix

--- script.js
+++ script.js
@@ -1,22 +1,27 @@
 //This function adds the animation to the non-decimals
 var nondecimalAnimation = function () {
-    $('#counter1').addClass('counter1');
-    $('#counter2').addClass('counter2');
-    $('#counter3').addClass('counter3');
-}
+  $("#counter1").addClass("counter1");
+  $("#counter2").addClass("counter2");
+  $("#counter3").addClass("counter3");
+};
 
 var runAnimationSection = function (the_id_of_element, the_function_to_run) {
-    //Animations for the section "Numbers"
-    var section = $(the_id_of_element);
-    var visibility = VisSense(section[0], { fullyvisible: 0.25 });
+  //Animations for the section "Numbers"
+  var section = $(the_id_of_element);
+  var visibility = VisSense(section[0], { fullyvisible: 0.25 });
 
-    visibility.monitor({
-        fullyvisible: function () {
-            if (section && section[0]) {
-                the_function_to_run();
-            }
-        },
-        hidden: function () {
+  visibility
+    .monitor({
+      fullyvisible: function () {
+        if (section && section[0]) {
+          the_function_to_run();
         }
-    }).start();
-}
+      },
+      hidden: function () {},
+    })
+    .start();
+};
+
+$(document).ready(function () {
+  runAnimationSection("#section-numbers", nondecimalAnimation);
+});
